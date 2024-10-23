e6a80b6d752e8d896ae1d3a328c34a43ae99cd7a
antomanu95 <antoniomr.estevez@hotmail.com>
2024-10-23 21:44:43 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,58 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // document.querySelector("card").innerHTML = generateRandomNumber;
+  // document.querySelector("card").classList.add(generateRandomSuit);
+  let valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let figura = ["♣", "♦", "♥", "♠"];
+  let randomNumber = Math.floor(Math.random() * 13);
+  let randomSuit = Math.floor(Math.random() * 4);
+
+  document.getElementById("iconoUp").innerHTML = figura[randomSuit];
+  document.getElementById("cardValue").innerHTML = valores[randomNumber];
+  document.getElementById("iconoDown").innerHTML = figura[randomSuit];
+
+  // document.console.log("Hello Rigo from the console!");
 };
+
+// // let generateRandomNumber = () => {
+//   // let valores = [
+//     "A",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "10",
+//     "J",
+//     "Q",
+//     "K"
+//   ];
+
+//   let numerosIndex = Math.floor(Math.random) * valores.length;
+//   return valores[numerosIndex];
+// };
+
+// let generateRandomSuit = () => {
+//   let figura = ["diamonds", "spades", "hearts", "clubs"];
+//   let figuraIndex = Math.floor(Math.random) * figura.length;
+//   return figura[figuraIndex];
+// // };
